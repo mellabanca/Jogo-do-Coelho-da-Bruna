2f9f3f22351999239e576fe82c50015c67a6def3
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-09-26 16:08:09 -0300

    26/09/2022

--- sketch.js
+++ sketch.js
@@ -10,6 +10,18 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var pizo;
+var korda;
+var futcha;
+var lassos;
+var backgroundI;
+var melonSugar;
+var sansao;
+
+function preload(){
+  backgroundI = loadImage("toyStory.jpg");
+  melonSugar = loadImage("melon.png");
+  sansao = loadImage("Rabbit-01.png");
+}
 
 function setup() 
 {
@@ -19,16 +31,24 @@ function setup()
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
+  imageMode(CENTER);
   textSize(50);
 
   pizo = new Chao(200, 690, 600, 20);
+  korda = new Rope (6, {x:245, y:40});
+  futcha = Bodies.circle (300, 300, 15);
+  Matter.Composite.add(korda.body,futcha);
+  lassos = new Vinculo (korda,futcha);
 }
 
 function draw() 
 {
   background(51);
+  image(backgroundI, width/2, height/2, 500, 700);
   Engine.update(engine);
   pizo.chou();
+  korda.chou();
+  image(melonSugar,futcha.position.x, futcha.position.y,60,60);
 }
 
 
---
+++ vinculo.js
@@ -0,0 +1,14 @@
+class Vinculo {
+constructor (corpo1,corpo2){
+var ligacao = corpo1.body.bodies.length-2;
+this.vinculo = Constraint.create ({
+bodyA: corpo1.body.bodies[ligacao],
+pointA: {x:0,y:0},
+bodyB: corpo2,
+pointB: {x:0,y:0},
+length: -10,
+stiffness: 0.01
+})
+World.add (engine.world,this.vinculo)
+}
+}
