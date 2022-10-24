75ac4874e4c840be907024faed50bf897f7c8de2
mellabanca <91677889+mellabanca@users.noreply.github.com>
2022-10-24 16:09:43 -0300

    24/10/2022

--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ var happiness;
 var ventinho;
 var pisca;
 var buchinho;
+var tristeza;
 
 function preload(){
   backgroundI = loadImage("toyStory.jpg");
@@ -32,8 +33,14 @@ function preload(){
   sansao = loadImage("Rabbit-01.png");
   pisca = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
   buchinho = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png");
-
+  tristeza = loadAnimation ("sad_1.png","sad_2.png","sad_3.png");
+  backyardigans = loadSound("sound1.mp3");
+  cut = loadSound("rope_cut.mp3");
+  sadness = loadSound("sad.wav");
+  happiness = loadSound("eating_sound.mp3");
+  ventinho = loadSound("air.wav");
   buchinho.looping = false;
+  tristeza.looping = false;
 }
 
 function setup()
@@ -51,6 +58,7 @@ function setup()
 
   pisca.frameDelay = 15;
   buchinho.frameDelay = 15;
+  tristeza.frameDelay = 15;
 
   pizo = new Chao(200, 690, 600, 20);
   korda = new Rope (6, {x:245, y:40});
@@ -62,6 +70,7 @@ function setup()
   pomposo.scale = 0.4
   pomposo.addAnimation("piscando", pisca);
   pomposo.addAnimation("comendo", buchinho);
+  pomposo.addAnimation("bolado",tristeza);
   pomposo.changeAnimation("piscando");
   enter = createImg ("cut_btn.png");
   enter.position(220,30);
@@ -76,10 +85,18 @@ function draw()
   Engine.update(engine);
   pizo.chou();
   korda.chou();
+  if(futcha!==null){
   image(melonSugar,futcha.position.x, futcha.position.y,63,63);
+
+  }
   drawSprites();
-
-
+  if(colissao(futcha,pomposo)===true){
+  pomposo.changeAnimation("comendo")
+  }
+  if(futcha !== null && futcha.position.y >= 650){
+    pomposo.changeAnimation("bolado")
+    futcha = null
+  }
 
 }
 
@@ -91,4 +108,17 @@ lassos=null;
 
 }
 
-
+function colissao(corpo,sprite){
+if(corpo!==null){
+var distanssia = dist(corpo.position.x,corpo.position.y,sprite.position.x, sprite.position.y) 
+if( distanssia <=80)
+{
+World.remove (engine.world,futcha);
+futcha = null
+return true
+}
+else {
+return false
+}
+}
+}
